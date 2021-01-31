afb2e838b35ebc2269547395c4d3ef522fff24e4
suissan <programming0607@gmail.com>
2021-01-31 10:57:19 +0900

    Herokuの環境変数を利用できるように修正

--- app/entry.js
+++ app/entry.js
@@ -33,3 +33,26 @@ $('.form-button').each((i, e) => {
     }
   });
 });
+
+$(() => {
+  var topBtn = $('.to-top');
+  //ボタンを非表示にする
+  topBtn.hide();
+  //スクロールしてページトップから100に達したらボタンを表示
+  $(window).scroll(() => {
+    if ($(this).scrollTop() > 100) {
+      //フェードインで表示
+      topBtn.fadeIn();
+    } else {
+      //フェードアウトで非表示
+      topBtn.fadeOut();
+    }
+  });
+  //スクロールしてトップへ戻る
+  topBtn.click(() => {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+});
